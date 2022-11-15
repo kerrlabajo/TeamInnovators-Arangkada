4eb19c15defce38c8399379fb92e8c5b08405e73
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-11-15 21:40:39 +0800

    Add springboot project

---
+++ Arangkada/src/test/java/com/innovators/Arangkada/ArangkadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovators.Arangkada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArangkadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
