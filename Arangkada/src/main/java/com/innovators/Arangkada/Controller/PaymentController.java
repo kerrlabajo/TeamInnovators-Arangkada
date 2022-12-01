a283e048fd65230356358b31a5e911ab8851ae2e
Kerr L. Labajo <kerr.labajo@cit.edu>
2022-12-01 09:21:32 +0800

    Add CrossOrigin annotation with local url

--- Arangkada/src/main/java/com/innovators/Arangkada/Controller/PaymentController.java
+++ Arangkada/src/main/java/com/innovators/Arangkada/Controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.innovators.Arangkada.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.innovators.Arangkada.Service.PaymentService;
 
 @RestController
 @RequestMapping("/payment")
+@CrossOrigin("http://localhost:3000/")
 public class PaymentController {
 
 	@Autowired
