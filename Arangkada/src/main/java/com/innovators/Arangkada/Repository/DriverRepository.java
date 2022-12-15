16e75f7cd594f8a575d5fc7505f25b5c012f9f8a
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-16 07:21:22 +0800

    Add changes to Driver Repository

--- Arangkada/src/main/java/com/innovators/Arangkada/Repository/DriverRepository.java
+++ Arangkada/src/main/java/com/innovators/Arangkada/Repository/DriverRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.innovators.Arangkada.Entity.DriverEntity;
-import com.innovators.Arangkada.Entity.OperatorEntity;
 
 
 @Repository
