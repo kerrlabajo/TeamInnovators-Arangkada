e58dd765a1684745df83b1a15902c183a8f383e0
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-16 07:33:10 +0800

    Add changes to Driver Service

--- Arangkada/src/main/java/com/innovators/Arangkada/Service/DriverService.java
+++ Arangkada/src/main/java/com/innovators/Arangkada/Service/DriverService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.innovators.Arangkada.Entity.DriverEntity;
-import com.innovators.Arangkada.Entity.OperatorEntity;
 import com.innovators.Arangkada.Repository.DriverRepository;
 
 @Service
