0a4d854cee5cda553a60a0defad1fe61e03d3bad
karyllejaycaballero <karyllejay.caballero@cit.edu>
2022-12-16 07:20:35 +0800

    Modify Driver Repository

--- Arangkada/src/main/java/com/innovators/Arangkada/Repository/DriverRepository.java
+++ Arangkada/src/main/java/com/innovators/Arangkada/Repository/DriverRepository.java
@@ -1,15 +1,18 @@
 package com.innovators.Arangkada.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.innovators.Arangkada.Entity.DriverEntity;
-
+import com.innovators.Arangkada.Entity.OperatorEntity;
 
 
 @Repository
 public interface DriverRepository extends JpaRepository<DriverEntity, Integer> {
 	
-	DriverEntity findByDriverId(int driverId);
+	DriverEntity findByLicenseNumber(String licenseNumber);
+	List<DriverEntity> findByAccountAccountId(int accountId);
 
 }
