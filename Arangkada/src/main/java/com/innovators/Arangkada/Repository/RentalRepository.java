bb044d343fab0ef071754ce03db6c4217fd1233c
Faith Rosalijos <faithrosalijos@gmail.com>
2022-12-16 11:36:09 +0800

    Add status update

--- Arangkada/src/main/java/com/innovators/Arangkada/Repository/RentalRepository.java
+++ Arangkada/src/main/java/com/innovators/Arangkada/Repository/RentalRepository.java
@@ -3,6 +3,8 @@ package com.innovators.Arangkada.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.innovators.Arangkada.Entity.RentalEntity;
@@ -16,4 +18,18 @@ public interface RentalRepository extends JpaRepository<RentalEntity, Integer>{
 	public List<RentalEntity> findByDriverDriverId(int id);
 	public List<RentalEntity> findByCurrentAndStatusIsNotAndVehicleOperatorOperatorId(boolean current, RentalStatus status, int id);
 	public RentalEntity findByCurrentAndDriverDriverId(boolean current, int id);
+	
+	@Modifying
+	@Query(
+		value = "UPDATE tbl_rental SET status = 'FINISHED' WHERE status = 'APPROVED' AND current = 1 AND end_date < CURDATE();",
+		nativeQuery = true	
+	)
+	public void updateStatusFinished();
+	
+	@Modifying
+	@Query(
+		value = "UPDATE tbl_rental SET status = 'EXPIRED', current = false WHERE status = 'PENDING' AND current = 1 AND end_date < CURDATE();",
+		nativeQuery = true	
+	)
+	public void updateStatusExpired();
 }
